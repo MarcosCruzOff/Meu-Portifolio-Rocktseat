870630eb22079d063e2ab0e161d2b20efc9b00e0
MarcosCruzOff <marcoscruz_off@outlook.com>
2022-02-14 20:13:49 -0300

    14/12/22- arrumando o css

--- server.js
+++ server.js
@@ -9,47 +9,53 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 server.get('/', function (req, res) {
-  const data = {
-    avatar_url: '/Eu2.jpg',
-    name: 'Marcos Coutinho',
-    role: 'Programador Junior Full Stack',
-    description1: 'Aspirante em Design UX/UI',
-    description2: 'Ouro 1 no League of Legends',
-    link: [
-      { name: 'Facebook', url: '#' },
-      { name: 'Instagram', url: '#' },
-      { name: 'Linkedin', url: '#' },
-      { name: 'GitHub', url: '#' },
-      { name: 'Email', url: '#' },
-      { name: 'Telefone', url: '#' }
-    ]
-  }
+   const data = {
+      avatar_url: '/Eu2.jpg',
+      name: 'Marcos Coutinho',
+      role: 'Programador Junior Full Stack',
+      description1: 'Aspirante em Design UX/UI',
+      description2: 'Ouro 1 no League of Legends',
+      link: [
+         { name: 'Facebook', url: 'https://www.facebook.com/marcoscruz.off' },
+         {
+            name: 'Instagram',
+            url: 'https://www.instagram.com/marcoscruz.off/'
+         },
+         {
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/in/marcos-cruz-8b9024224/'
+         },
+         { name: 'GitHub', url: 'https://github.com/MarcosCruzOff' },
+         { name: 'Email', url: 'marcoscruz673@gmail.com' },
+         { name: 'Telefone', url: '+5591986172759' }
+      ]
+   }
 
-  return res.render('home', { data: data })
+   return res.render('home', { data: data })
 })
 
 server.get('/trabalho', function (req, res) {
-  return res.render('trabalho', { items: videos })
+   return res.render('trabalho', { items: videos })
 })
 
 server.get('/video', function (req, res) {
-  const id = req.query.id
+   const id = req.query.id
 
-  const video = videos.find(function (video) {
-    return video.id == id
-  })
+   const video = videos.find(function (video) {
+      return video.id == id
+   })
 
-  if (!video) {
-    return res.send('Não Encontrou')
-  }
+   if (!video) {
+      return res.send('Não Encontrou')
+   }
 
-  return res.render('video', { item: video })
+   return res.render('video', { item: video })
 })
 
 server.listen(5000, function () {
-  console.log('funcionou')
+   console.log('funcionou')
 })
 
 nunjucks.configure('views', {
-  express: server
+   express: server
 })
